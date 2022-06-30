2293808fb76916d11064ce0daaa0e8f47a87b1f8
Clement Azalbert <clementazalbert04@gmail.com>
2022-06-30 18:14:44 +0200

    script.js done

---
+++ script.js
@@ -0,0 +1,35 @@
+const video = document.querySelector("video");
+const textElem = document.querySelector("[data-text")
+
+async function setup() {
+   const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+   video.srcObject = stream;
+
+   // Check if video playing
+   video.addEventListener("playing", async () => {
+    const worker = Tesseract.createWorker();
+    await worker.load();
+    await worker.loadLanguage("fr");
+    await worker.initialize("fr");
+
+    // Create the canvas
+    const canvas = document.createElement("canvas");
+    canvas.width = video.width;
+    canvas.height = video.height;
+
+    // Check for keypress and put the video in the canvas
+    document.addEventListener("keypress", async event => {
+        if (event.code !== "Space") return
+        canvas.getContext("2d").drawImage(video, 0 , 0, video.width, video.height)
+        const {
+            data : { text }
+        } = await worker.recognize(canvas);
+
+        speechSynthesis.speak(new SpeechSynthesisUtterance(text.replace(/\s/g, " ")));
+
+        textElem.textContent = text;
+    })
+   })
+}
+
+setup();
